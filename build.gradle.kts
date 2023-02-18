43f8f771c593517ed17a48b314c670aa658a2083
wcaleniekubaa <87438086+wcaleniekubaa@users.noreply.github.com>
2023-02-18 22:09:52 +0100

    Implement Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.21"
-    application
+   kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.dokka") version "1.6.10"
 }
 
 group = "tk.teaclient"
@@ -15,13 +13,11 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.19.0")
     implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-
     implementation("org.apache.httpcomponents:httpclient:4.5.14")
     implementation("com.google.code.gson:gson:2.10")
 }
 
-
-
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
+
